a0c1d78f222ad79a8ee9db4a15a6b6bc58213447
--help <iuli.zacarias@gmail.com>
2018-07-09 14:17:25 -0300

    Renamed 10 nodes scenario.

--- simplewifi10.py
+++ simplewifi10.py
@@ -3,7 +3,6 @@
 'This example creates a simple network topology with 1 AP and 2 stations'
 
 import sys
-
 from mininet.node import Controller
 from mininet.log import setLogLevel, info
 from mininet.term import makeTerm
@@ -12,16 +11,12 @@ from mininet.wifi.cli import CLI_wifi
 from mininet.wifi.net import Mininet_wifi
 
 
-def topology(isVirtual):
+def topology():
     "Create a network."
     net = Mininet_wifi(controller=Controller, accessPoint=OVSKernelAP)
 
     info("*** Creating nodes\n")
-    if isVirtual:
-        sta1 = net.addStation('sta1', nvif=2)
-    else:
-        sta1 = net.addStation('sta1')
-    " Creating network statitions"
+    sta1 = net.addStation('sta1')
     sta2 = net.addStation('sta2')
     sta3 = net.addStation('sta3')
     sta4 = net.addStation('sta4')
@@ -30,6 +25,8 @@ def topology(isVirtual):
     sta7 = net.addStation('sta7')
     sta8 = net.addStation('sta8')
     sta9 = net.addStation('sta9')
+    sta10 = net.addStation('sta10')
+
     ap1 = net.addAccessPoint('ap1', ssid="simplewifi", mode="g", channel="5")
     c0 = net.addController('c0', controller=Controller, ip='127.0.0.1',
                            port=6633)
@@ -47,6 +44,7 @@ def topology(isVirtual):
     net.addLink(sta7, ap1)
     net.addLink(sta8, ap1)
     net.addLink(sta9, ap1)
+    net.addLink(sta10, ap1)
 
     info("*** Starting network\n")
     net.build()
@@ -57,15 +55,16 @@ def topology(isVirtual):
     net.pingAll()
 
     # print "Sta1 IP: %s" % sta1.params['ip'][0].split('/')[0]
-    makeTerm( sta1, cmd="python /media/sf_shared/node.py 10.0.0.1;read" )
-    makeTerm( sta2, cmd="python /media/sf_shared/node.py 10.0.0.2;read" )
-    makeTerm( sta3, cmd="python /media/sf_shared/node.py 10.0.0.3;read" )
-    makeTerm( sta4, cmd="python /media/sf_shared/node.py 10.0.0.4;read" )
-    makeTerm( sta5, cmd="python /media/sf_shared/node.py 10.0.0.5;read" )
-    makeTerm( sta6, cmd="python /media/sf_shared/node.py 10.0.0.6;read" )
-    makeTerm( sta7, cmd="python /media/sf_shared/node.py 10.0.0.7;read" )
-    makeTerm( sta8, cmd="python /media/sf_shared/node.py 10.0.0.8;read" )
-    makeTerm( sta9, cmd="python /media/sf_shared/node.py 10.0.0.9;read" )
+    makeTerm( sta1, cmd="python /media/sf_shared/node.py 10.0.0.1 -nodes 10 -rep 1;read" )
+    makeTerm( sta2, cmd="python /media/sf_shared/node.py 10.0.0.2 -nodes 10 -rep 1;read" )
+    makeTerm( sta3, cmd="python /media/sf_shared/node.py 10.0.0.3 -nodes 10 -rep 1;read" )
+    makeTerm( sta4, cmd="python /media/sf_shared/node.py 10.0.0.4 -nodes 10 -rep 1;read" )
+    makeTerm( sta5, cmd="python /media/sf_shared/node.py 10.0.0.5 -nodes 10 -rep 1;read" )
+    makeTerm( sta6, cmd="python /media/sf_shared/node.py 10.0.0.6 -nodes 10 -rep 1;read" )
+    makeTerm( sta7, cmd="python /media/sf_shared/node.py 10.0.0.7 -nodes 10 -rep 1;read" )
+    makeTerm( sta8, cmd="python /media/sf_shared/node.py 10.0.0.8 -nodes 10 -rep 1;read" )
+    makeTerm( sta9, cmd="python /media/sf_shared/node.py 10.0.0.9 -nodes 10 -rep 1;read" )
+    makeTerm( sta10, cmd="python /media/sf_shared/node.py 10.0.0.10 -nodes 10 -rep 1;read" )
 
     info("*** Running CLI\n")
     CLI_wifi(net)
@@ -76,5 +75,4 @@ def topology(isVirtual):
 
 if __name__ == '__main__':
     setLogLevel('info')
-    isVirtual = True if '-v' in sys.argv else False
-    topology(isVirtual)
+    topology()
